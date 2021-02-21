da5c2916bf47e7a41801e8575ecbf2c796c9a918
Jesse Wenger <wengerj@gmail.com>
2021-02-21 20:21:10 +0000

    Automatically backed up by Learn

--- ask_person_name.rb
+++ ask_person_name.rb
@@ -1,9 +1,4 @@
-#require_relative './say_hello.rb'
+require_relative './say_hello.rb'
 
-def ask_person_name
-  
   puts "What is your name?"
-  
-  #person_name = gets.to_s
-  
-end
+  person_name = gets.to_s
